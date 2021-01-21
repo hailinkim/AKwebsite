d2891bfe6032bf624076a9545b373c2bd299b0d5
hailinkim <40394797+hailinkim@users.noreply.github.com>
2021-01-21 09:31:25 +0900

    JS: tweak navbar when scrolled

--- main.js
+++ main.js
@@ -25,12 +25,15 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
 
+    navbarMenu.classList.remove('open');
+
     //Remove selection 
-    const active = document.querySelector('.navbar__menu__item.selected');
-    active.classList.remove('selected');
-    event.target.classList.add('selected');
+    const active = document.querySelector('.navbar__menu__item.active');
+    active.classList.remove('active');
+    event.target.classList.add('active');
 
     scrollIntoView(link);
+    selectNavItem(target);
 });
 
 //Navbar toggle button
@@ -42,7 +45,7 @@ navbarToggleBtn.addEventListener('click', () => {
 //Navbar toggle animation
 function myFunction(x) {
     x.classList.toggle("change");
-  }
+}
 
 //Make about gradually fade to transparent as the window scolls down
 const about = document.querySelector('.about__container');
@@ -68,3 +71,52 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+const sectionIds = ['#about', '#skills', '#portfolio', '#contact'];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+console.log(sections);
+console.log(navItems);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected){
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+    root: null,
+    rootMargin : '0px',
+    threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        // console.log(entry.target);
+        if(!entry.isIntersecting && entry.intersectionRatio >0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            
+            // console.log(index, entry.target.id)
+            //scorlled down, page comes up
+            if(entry.boundingClientRect.y < 0 ){
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex = index - 1;
+            }
+            
+        }
+    });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if(window.scrollY===0){
+        selectedNavIndex = 0;
+    } else if (window.scrollY + window.innerHeight === document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+})
